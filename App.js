85e14c2f8a5594427aca755675027d9062e5f623
Seraj Lee <Lucas_Naruto_AE@Yahoo.com>
2021-01-04 09:16:12 -0800

    Added Chat navigation screen

--- App.js
+++ App.js
@@ -1,6 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from 'react-navigation';
+import Chat from './components/Chat';
+
+export default createStackNavigator({
+  Chat: { screen: Chat }
+});
 
 export default function App() {
   return (
