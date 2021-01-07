ab7c6a2f1ced7c3ee3b7e5a75f848d39e320e358
Seraj Lee <Lucas_Naruto_AE@Yahoo.com>
2021-01-07 12:15:41 -0800

    Stable App Version 1.0

--- App.js
+++ App.js
@@ -1,5 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import HomeScreen from './src/screens/Home';
+import ChatScreen from './src/screens/Chat';
+import LoginScreen from './src/screens/Login';
+
+const RootStack = createStackNavigator();
  
 const styles = StyleSheet.create({
   container: {
@@ -11,9 +19,13 @@ const styles = StyleSheet.create({
  
 const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Hello React Native!</Text>
-    </View>
+    <NavigationContainer>
+      <RootStack.Navigator>
+        <RootStack.Screen name="Login" component={LoginScreen} />
+        <RootStack.Screen name="Home" component={HomeScreen} />
+        <RootStack.Screen name="Chat" component={ChatScreen} />
+      </RootStack.Navigator>
+    </NavigationContainer>
   );
 };
  
