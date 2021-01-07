e2f9b867e62d9b60af2da11aeef4b1bff9b40887
Seraj Lee <Lucas_Naruto_AE@Yahoo.com>
2021-01-07 14:25:23 -0800

    Key removal

--- src/screens/Config/firebaseSDK.js
+++ src/screens/Config/firebaseSDK.js
@@ -9,14 +9,14 @@ class FirebaseSDK {
   init = () => {
     if (!firebase.apps.length) {
       firebase.initializeApp({
-        apiKey: 'AIzaSyA8Dp8yInWfJV_qUQdgZfknhZXDeeENFzg',
-        authDomain: 'naxechat.firebaseapp.com',
-        databaseURL: 'https://naxechat-default-rtdb.firebaseio.com',
-        projectId: 'naxechat',
-        storageBucket: 'naxechat.appspot.com',
-        messagingSenderId: '23143048525',
-        appId: "1:23143048525:web:3b9d70f2db9de05796c304",
-        measurementId: "G-TTQSG6KSY3"
+        apiKey: '',
+        authDomain: '',
+        databaseURL: '',
+        projectId: '',
+        storageBucket: '',
+        messagingSenderId: '',
+        appId: "",
+        measurementId: ""
       });
     }
   };
