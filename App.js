523f4fa38f8a6929146317d2a5e221677e33e0d4
Seraj Lee <Lucas_Naruto_AE@Yahoo.com>
2021-01-23 12:23:07 -0800

    Implemented a Signup screen

--- App.js
+++ App.js
@@ -3,9 +3,10 @@ import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
+import LoginScreen from './src/screens/Login';
+import SignupScreen from './src/screens/Signup';
 import HomeScreen from './src/screens/Home';
 import ChatScreen from './src/screens/Chat';
-import LoginScreen from './src/screens/Login';
 
 const navigation = createStackNavigator();
  
@@ -22,6 +23,7 @@ const App = () => {
     <NavigationContainer>
       <navigation.Navigator>
         <navigation.Screen name="Login" component={LoginScreen} />
+        <navigation.Screen name="Signup" component={SignupScreen} />
         <navigation.Screen name="Home" component={HomeScreen} />
         <navigation.Screen name="Chat" component={ChatScreen} />
       </navigation.Navigator>
